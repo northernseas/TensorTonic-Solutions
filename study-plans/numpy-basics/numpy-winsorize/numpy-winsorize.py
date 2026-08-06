939111cd1586377b81ff9cfadf595fa123e6c35b
northernseas <t@theolepage.com>
2026-08-06 16:54:57 +0200

    Solve numpy-winsorize

--- study-plans/numpy-basics/numpy-winsorize/numpy-winsorize.py
+++ study-plans/numpy-basics/numpy-winsorize/numpy-winsorize.py
@@ -3,16 +3,14 @@ import numpy as np
 def winsorize(data, lo_q, hi_q):
     """Returns: np.ndarray of shape (3, m, n), stacked clipped values, lo_mask, hi_mask"""
     data = np.array(data, dtype=np.float64)
-    lo_q = np.percentile(data, q=lo_q, axis=0)
-    hi_q = np.percentile(data, q=hi_q, axis=0)
 
-    data_new = np.clip(data, lo_q, hi_q)
+    lo = np.percentile(data, q=lo_q, axis=0)
+    hi = np.percentile(data, q=hi_q, axis=0)
 
-    lower = np.where(data < lo_q, 1, 0)
-    higher = np.where(data > hi_q, 1, 0)
+    clipped = np.clip(data, lo, hi)
     
     return np.stack((
-        data_new,
-        lower,
-        higher
+        clipped,
+        data < lo,
+        data > hi
     ))

61b48e2ce94ead3f61f1398b7923dacf9d4ef6f4
northernseas <t@theolepage.com>
2026-08-06 17:01:28 +0200

    Solve numpy-quantize-frame

--- study-plans/numpy-basics/numpy-quantize-frame/numpy-quantize-frame.py
+++ study-plans/numpy-basics/numpy-quantize-frame/numpy-quantize-frame.py
@@ -3,8 +3,9 @@ import numpy as np
 def quantize_and_frame(data, decimals, pad_width):
     """Returns: np.ndarray of shape (3, m+2p, n+2p), stacked rounded, floored, ceiled with zero-padding"""
     data = np.array(data, dtype=np.float64)
+    pad = lambda x: np.pad(x, pad_width, mode="constant", constant_values=0.0)
     return np.stack((
-        np.pad(np.round(data, decimals), pad_width, mode="constant"),
-        np.pad(np.floor(data), pad_width, mode="constant"),
-        np.pad(np.ceil(data), pad_width, mode="constant"),
+        pad(np.round(data, decimals)),
+        pad(np.floor(data)),
+        pad(np.ceil(data)),
     ))

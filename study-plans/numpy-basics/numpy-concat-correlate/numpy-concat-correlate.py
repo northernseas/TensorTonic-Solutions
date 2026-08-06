6134c23a1ae7fff114f4e371b32d9b1a5bd43d04
northernseas <t@theolepage.com>
2026-08-06 17:15:09 +0200

    Solve numpy-concat-correlate

---
+++ study-plans/numpy-basics/numpy-concat-correlate/numpy-concat-correlate.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def compare_correlations(a, b):
+    """Returns: np.ndarray of shape (3, n, n), stacked correlation matrices"""
+    a = np.array(a, dtype=np.float64)
+    b = np.array(b, dtype=np.float64)
+    combined = np.concatenate((a, b), axis=0)
+    return np.stack((
+        np.corrcoef(a.T),
+        np.corrcoef(b.T),
+        np.corrcoef(combined.T),
+    ))

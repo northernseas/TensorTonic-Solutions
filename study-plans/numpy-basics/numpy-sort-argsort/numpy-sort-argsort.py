cdd57fc765c7d161552f4a085046e419350d44dd
northernseas <t@theolepage.com>
2026-08-06 16:05:00 +0200

    Solve numpy-sort-argsort

--- study-plans/numpy-basics/numpy-sort-argsort/numpy-sort-argsort.py
+++ study-plans/numpy-basics/numpy-sort-argsort/numpy-sort-argsort.py
@@ -3,9 +3,7 @@ import numpy as np
 def sort_with_indices(data, axis):
     """Returns: np.ndarray of shape (2, m, n), stacked sorted values and sort indices"""
     data = np.array(data, dtype=np.float64)
-    values = np.sort(data, axis)
     idx = np.argsort(data, axis)
-    return np.stack((
-        values,
-        idx
-    ))
+    # values = np.sort(data, axis)
+    values = np.take_along_axis(data, idx, axis)
+    return np.stack((values, idx))

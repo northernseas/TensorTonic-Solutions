ceba66bcbe3f3f3a24bd1be42297a4d320897739
northernseas <t@theolepage.com>
2026-08-07 10:23:52 +0200

    Solve pytorch-custom-linear-layer

--- study-plans/pytorch-basics/pytorch-custom-linear-layer/pytorch-custom-linear-layer.py
+++ study-plans/pytorch-basics/pytorch-custom-linear-layer/pytorch-custom-linear-layer.py
@@ -12,5 +12,8 @@ class CustomLinear(nn.Module):
         self.weight = nn.Parameter(torch.empty(out_features, in_features), requires_grad=True)
         self.bias = nn.Parameter(torch.empty(out_features), requires_grad=True)
 
+        nn.init.kaiming_normal_(self.weight)
+        nn.init.constant_(self.bias, 0)
+
     def forward(self, x):
         return x @ self.weight.T + self.bias

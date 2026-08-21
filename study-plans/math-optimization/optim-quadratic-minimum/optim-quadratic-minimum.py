a220b8c287afb2e392d6dc609085ea0e0c54cf44
northernseas <t@theolepage.com>
2026-08-21 10:28:51 +0200

    Solve optim-quadratic-minimum

---
+++ study-plans/math-optimization/optim-quadratic-minimum/optim-quadratic-minimum.py
@@ -0,0 +1,11 @@
+def quadratic_minimum(a, b, c):
+    """
+    Returns: dict with 'x_star' and 'f_min' (floats), each rounded to 6 decimals
+    """
+    x_star = -b / (2 * a)
+    f_min = a * x_star ** 2 + b * x_star + c
+
+    return {
+        "x_star": x_star,
+        "f_min": f_min,
+    }

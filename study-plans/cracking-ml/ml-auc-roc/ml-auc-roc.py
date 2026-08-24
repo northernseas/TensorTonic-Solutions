7d96e576be93776a1f8e1938ec9fd8b3d741a67a
northernseas <t@theolepage.com>
2026-08-24 10:35:58 +0200

    Solve ml-auc-roc

--- study-plans/cracking-ml/ml-auc-roc/ml-auc-roc.py
+++ study-plans/cracking-ml/ml-auc-roc/ml-auc-roc.py
@@ -4,28 +4,25 @@ def auc_roc(y_true, y_scores):
     """
     Returns: tuple of (fpr_list, tpr_list, auc_value)
     """
-    y_true = np.array(y_true)
+    y_true = np.array(y_true, dtype=bool)
     y_scores = np.array(y_scores, dtype=np.float64)
-
-    # idx = np.argsort(y_scores)[::-1]
-    # y_true = y_true[idx]
-    # y_scores = y_scores[idx]
 
     thresholds = np.unique(y_scores)[::-1]
     
     tpr = [0]
     fpr = [0]
+
+    nb_positives = np.sum(y_true)
+    nb_negatives = np.sum(~y_true)
     
     for threshold in thresholds:
-        y_pred = y_scores >= threshold
+        y_pred = (y_scores >= threshold).astype(bool)
 
-        tp = np.sum((y_pred == 1) & (y_true == 1))
-        tn = np.sum((y_pred == 0) & (y_true == 0))
-        fp = np.sum((y_pred == 1) & (y_true == 0))
-        fn = np.sum((y_pred == 0) & (y_true == 1))
-        
-        tpr.append(tp / (tp + fn))
-        fpr.append(fp / (fp + tn))
+        tp = np.sum(y_pred & y_true)
+        fp = np.sum(y_pred & ~y_true)
+
+        tpr.append(tp / nb_positives)
+        fpr.append(fp / nb_negatives)
         
     auc = 0
     for i in range(1, len(tpr)):
